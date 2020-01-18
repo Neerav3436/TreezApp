ebc29b5f3c3a060d89ddbda85d79e7eb4ca4e105
neerav3436 <neeravpatel85@gmail.com>
2020-01-18 17:00:12 -0500

    BoilerPlate code with Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SortingComponent } from './sorting-component/sorting.component';
-import { PalindromeComponent } from './palindrome-component/palindrome.component';
-import { ErrorPageComponent } from './error-page-component/error-page.component';
-import { AnagramComponent } from './anagram-component/anagram.component';
+
 import { AnagramComponent } from './anagram/anagram.component';
-import { ErrorPageComponent } from './error-page/error-page.component';
-import { PalindromeComponent } from './palindrome/palindrome.component';
 import { SortingComponent } from './sorting/sorting.component';
+import { PalindromeComponent } from './palindrome/palindrome.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 
 const appRoutes: Routes = [
-  { path: '', component: SortingComponent },
+  { path: 'sorting', component: SortingComponent },
   { path: 'palindrome', component: PalindromeComponent},
   { path: 'anagram', component: AnagramComponent},
   { path: 'not-found', component: ErrorPageComponent},
@@ -27,14 +24,11 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    SortingComponentComponent,
-    PalindromeComponentComponent,
-    ErrorPageComponentComponent,
-    AnagramComponentComponent,
-    AnagramComponent,
-    ErrorPageComponent,
+    SortingComponent,
     PalindromeComponent,
-    SortingComponent
+    ErrorPageComponent,
+    AnagramComponent,
+    AnagramComponent
   ],
   imports: [
     BrowserModule,
